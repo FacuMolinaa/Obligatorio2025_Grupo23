92ef5cb57a7e129363acc1ffcf21998dfe81d183
FacuMolinaa <facundomolina78@gmail.com>
2025-05-07 16:46:17 -0300

    Obligatorio_2025_Grupo23

---
+++ Obligatorio_2025_Grupo23.py
@@ -0,0 +1,6 @@
+class Sistema:
+    def __init__(self,clientes,piezas,maquinas):
+        self.clientes = clientes
+        self.piezas = piezas
+        self.maquinas = maquinas
+        self.ordenes = []
