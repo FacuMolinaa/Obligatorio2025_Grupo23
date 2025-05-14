4c4bd42309fcbc4c8844ab88f633f852642542ce
FacuMolinaa <135667039+FacuMolinaa@users.noreply.github.com>
2025-05-13 21:22:27 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+if __name__ == "__main__":
+
+    a = print ("Fábrica. Elija una opción:")
+    while True:
+        print ("---------------------------------------")
+        print ("1. Registrar")
+        if a == 1:
+            print ("1.1. Registrar pieza")
+            print ("1.2. Máquina")
+            print ("1.3. Cliente")
+            print ("1.4. Orden")
+            print ("1.5 Consultar")
+            print ("1.6. Salir")
+        print ("2. Listar")
+        if a == 2:
+            print ("2.1. Clientes")
+            print ("2.2. Pedidos")
+            print ("2.3. Máquinas")
+            print ("2.4. Piezas")
+            print ("2.5. Contabilidad")
+            print ("2.6. Salir")
+        print ("3. Maquinas")
+        if a == 3:
+            print ("Salir del sistema.")
+        print (")---------------------------------------")
